6450de8c1c1778859df556a151daeaf3c90b99ff
jeru <john.ungab7@gmail.com>
2025-02-06 11:31:16 +0800

    add: lucide-react, responsive nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,41 @@
-import { type ReactElement } from "react";
+import { useState, type ReactElement } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function Nav(): ReactElement {
+  // TODO: nav bar - mobile first
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="w-full bg-[var(--color-green-primary)] font-baloo font-bold h-20 flex items-center justify-between px-40 fixed top-0 z-10">
-      <ul className="flex gap-24 text-2xl text-white items-center">
-        <li className="transition-transform duration-200 hover:scale-105 cursor-pointer">MATHPATH</li>
-        <li className="transition-transform duration-200 hover:scale-105 cursor-pointer">ABOUT</li>
-        <li className="transition-transform duration-200 hover:scale-105 cursor-pointer">DOWNLOAD</li>
-      </ul>
-
-      <button className="bg-[var(--color-yellow)] border rounded-3xl py-2 px-8 hover:scale-105 cursor-pointer transition-transform duration-200">
-        <p className="text-xl">LOG IN</p>
-      </button>
-    </nav>
+    <>
+      <nav className="font-baloo w-full bg-[var(--color-green-primary)] p-5 text-white shadow md:flex md:items-center md:justify-between">
+        <div className="flex items-center justify-between">
+          <span className="cursor-pointer text-4xl font-bold">MathPath</span>
+          <span className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <X size={36} /> : <Menu size={36} />}
+          </span>
+        </div>
+        <ul className={
+          `absolute left-0 w-full bg-[var(--color-green-primary)] py-4 pl-3 md:static md:z-auto md:flex md:w-fit md:items-center md:py-0 md:pl-0 md:opacity-100 ${isOpen ? "opacity-100" : "hidden opacity-0"}`}>
+          <li className="mx-4 my-6 md:my-0">
+            <a
+              href="#"
+              className="hover:text-yellow text-xl duration-300 md:font-semibold"
+            >
+              ABOUT
+            </a>
+          </li>
+          <li className="mx-4 my-6 md:my-0">
+            <a
+              href="#"
+              className="hover:text-yellow text-xl duration-300 md:font-semibold"
+            >
+              DOWNLOAD
+            </a>
+          </li>
+          <button className="duration:500 rounded-3xl border bg-[var(--color-yellow)] px-6 py-2 text-xl font-bold text-black hover:scale-105 hover:cursor-pointer">
+            LOG IN
+          </button>
+        </ul>
+      </nav>
+    </>
   );
 }
--- src/pages/landing/About.tsx
+++ src/pages/landing/About.tsx
@@ -2,8 +2,7 @@ import { type ReactElement } from "react";
 
 export default function About(): ReactElement {
   return (
-    <section className="w-full h-screen p-24">
-      <div className="bg-[var(--color-green-secondary)] w-full h-full border-2 rounded-4xl"></div>
+    <section className="">
     </section>
   );
 }
