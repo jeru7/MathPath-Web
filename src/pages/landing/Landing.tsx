817ed94a08b6b46d85012eb830101c2b503dbea9
jeru <john.ungab7@gmail.com>
2025-03-17 09:57:55 +0800

    added: framer motion, edit: hero section  animations

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -8,42 +8,71 @@ import bgTrees from "../../assets/backgroundImage/bgTrees.png";
 import upperTrees from "../../assets/svgs/upperTrees.svg";
 import bottomBush from "../../assets/svgs/bottomBush.svg";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { motion } from "framer-motion";
 
 export default function Landing(): ReactElement {
   return (
     <div className="">
       <Parallax pages={2}>
         {/* Hero */}
+
+        {/* background trees */}
         <ParallaxLayer offset={0} speed={0}>
-          <div
+          <motion.div
             className="absolute inset-0 bg-cover bg-center"
             style={{
               backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0) 60%, rgba(0,0,0,0.8)), url(${bgTrees})`,
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
+            initial={{ scale: 1.1 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 2, ease: "easeOut" }}
           />
-          <div className="absolute bottom-0 left-0 h-80 w-full bg-gradient-to-t from-[#222222] to-transparent" />
+
+          {/* bottom fade - bg */}
+          <div className="absolute bottom-0 left-0 h-60 w-full bg-gradient-to-t from-[#222222] to-transparent" />
         </ParallaxLayer>
 
+        {/* upper trees */}
         <ParallaxLayer offset={0} speed={0.2}>
-          <div
-            className="absolute left-0 right-0 top-0 max-h-[40vh] min-h-[120px] w-full bg-cover bg-top md:bg-contain xl:bg-center"
+          <motion.div
+            className="absolute left-0 right-0 top-0 min-h-[120px] w-full bg-cover bg-top md:bg-contain xl:h-[20vh] xl:bg-center"
             style={{
               backgroundImage: `url(${upperTrees})`,
+              willChange: "transform",
+            }}
+            animate={{
+              y: [0, -5, 0],
+            }}
+            transition={{
+              duration: 3,
+              repeat: Infinity,
+              ease: "easeInOut",
             }}
           />
-          <div className="absolute left-0 top-0 h-24 w-full bg-gradient-to-b from-black/60 to-transparent" />
         </ParallaxLayer>
 
+        {/* bottom bush */}
         <ParallaxLayer offset={0} speed={0.1}>
-          <div
-            className="absolute bottom-0 left-1/2 h-[30vh] w-full -translate-x-1/2 bg-cover md:h-[20vh] md:bg-contain xl:bg-center"
+          <motion.div
+            className="md:bg-fill absolute bottom-0 left-1/2 h-[30vh] w-full -translate-x-1/2 bg-cover lg:bg-center"
             style={{
               backgroundImage: `url(${bottomBush})`,
+              willChange: "transform",
             }}
-          />
-          <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-[#222222] to-transparent" />
+            animate={{
+              y: [0, 5, 0],
+            }}
+            transition={{
+              duration: 5,
+              repeat: Infinity,
+              ease: "easeOut",
+            }}
+          >
+            {/* bottom fade - bush */}
+            <div className="absolute bottom-0 left-0 h-20 w-full bg-gradient-to-t from-[#222222] to-transparent" />
+          </motion.div>
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -51,7 +80,21 @@ export default function Landing(): ReactElement {
           speed={0.35}
           className="flex h-screen items-center justify-center"
         >
-          <Hero />
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: [0, -20, 0] }}
+            transition={{
+              opacity: { duration: 1, ease: "easeOut" },
+              y: {
+                duration: 4,
+                repeat: Infinity,
+                repeatType: "mirror",
+                ease: "easeInOut",
+              },
+            }}
+          >
+            <Hero />
+          </motion.div>
         </ParallaxLayer>
       </Parallax>
     </div>
