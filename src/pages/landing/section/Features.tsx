bcf732cd3d0db5431b5d3f19800626314b040500
jeru <john.ungab7@gmail.com>
2025-03-19 22:24:58 +0800

    add: game feature responsive layout

--- src/pages/landing/section/Features.tsx
+++ src/pages/landing/section/Features.tsx
@@ -2,6 +2,8 @@ import { type ReactElement } from "react";
 import samplePic from "../../../assets/images/samplePic.jpg";
 import GameFeatureCard from "./components/GameFeatureCard";
 import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
 
 const features = [
   {
@@ -24,6 +26,28 @@ const features = [
   },
 ];
 
+const mobileAnimationValue = {
+  mobile: {
+    delay: {
+      img: 0.2,
+      content: 0.2,
+    },
+  },
+  desktop: null,
+  onlyShowOnce: true,
+};
+
+const desktopAnimationValue = {
+  mobile: null,
+  desktop: {
+    delay: {
+      img: 0.6,
+      content: 0.2,
+    },
+  },
+  onlyShowOnce: false,
+};
+
 export default function Features(): ReactElement {
   return (
     <section className="flex h-fit w-screen flex-col items-center justify-center gap-12 bg-[var(--primary-black)] px-8 text-[var(--primary-white)]">
@@ -32,20 +56,48 @@ export default function Features(): ReactElement {
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
-        viewport={{once: true}}
+        viewport={{ once: true }}
       >
         Game Features
       </motion.h3>
-      <div className="flex flex-col gap-16">
+
+      {/* member cards mobile */}
+      <div className="flex h-fit w-full flex-col gap-16 lg:hidden">
         {features.map((feature) => (
           <GameFeatureCard
-            className=""
+            className="h-full w-full snap-center flex-col gap-6"
             imgSrc={feature.img}
             title={feature.title}
             description={feature.description}
+            animation={mobileAnimationValue}
           />
         ))}
       </div>
+
+      {/* member cards desktop */}
+      <Swiper
+        direction="vertical"
+        slidesPerView={1}
+        spaceBetween={64}
+        mousewheel={{ forceToAxis: true }}
+        touchStartPreventDefault={false}
+        touchMoveStopPropagation={true}
+        simulateTouch={true}
+        className="hidden h-[50vh] w-full overflow-y-hidden overscroll-none p-8 lg:flex"
+        modules={[Mousewheel]}
+      >
+        {features.map((feature, index) => (
+          <SwiperSlide key={index} className="h-full snap-center">
+            <GameFeatureCard
+              className="h-full w-full flex-row lg:gap-24 xl:px-32"
+              imgSrc={feature.img}
+              title={feature.title}
+              description={feature.description}
+              animation={desktopAnimationValue}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
--- src/pages/landing/section/components/GameFeatureCard.tsx
+++ src/pages/landing/section/components/GameFeatureCard.tsx
@@ -6,6 +6,21 @@ interface IGameFeatureCard {
   imgSrc: string;
   title: string;
   description: string;
+  animation: {
+    mobile?: {
+      delay: {
+        img: number;
+        content: number;
+      };
+    } | null;
+    desktop?: {
+      delay: {
+        img: number;
+        content: number;
+      };
+    } | null;
+    onlyShowOnce: boolean;
+  };
 }
 
 const GameFeatureCard: React.FC<IGameFeatureCard> = ({
@@ -13,41 +28,61 @@ const GameFeatureCard: React.FC<IGameFeatureCard> = ({
   imgSrc,
   title,
   description,
+  animation,
 }: IGameFeatureCard): ReactElement => {
   return (
     <div
-      className={`${className} flex h-fit w-full flex-col items-center justify-center gap-6 text-[var(--primary-white)]`}
+      className={`${className} flex h-full items-center justify-center text-[var(--primary-white)]`}
     >
       {/* image */}
       <motion.img
         src={imgSrc}
         alt={title}
-        className="h-auto w-[80%] rounded-lg"
-        initial={{ opacity: 0, y: 10 }}
+        className="lg:w[50%]  h-full w-[80%] rounded-2xl"
+        initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
-        viewport={{ once: true }}
+        transition={{
+          duration: 0.8,
+          ease: "easeOut",
+          delay:
+            animation.mobile?.delay.img ?? animation.desktop?.delay.img ?? 0.2,
+        }}
+        viewport={{ once: animation.onlyShowOnce }}
       />
 
       {/* title */}
       <div className="flex flex-col items-center gap-5">
         <motion.p
-          className="text-3xl font-bold"
+          className="w-fit text-3xl font-bold"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
-          viewport={{ once: true }}
+          transition={{
+            duration: 0.8,
+            ease: "easeOut",
+            delay:
+              animation.mobile?.delay.content ??
+              animation.desktop?.delay.content ??
+              0.2,
+          }}
+          viewport={{ once: animation.onlyShowOnce }}
         >
           {title}
         </motion.p>
 
         {/* description */}
         <motion.p
-          className="text-center text-lg"
+          className="w-[80%] text-center text-lg"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 0.6, y: 0 }}
-          transition={{ duration: 0.7, ease: "easeOut", delay: 0.2 }}
-          viewport={{ once: true }}
+          transition={{
+            duration: 0.7,
+            ease: "easeOut",
+            delay:
+              animation.mobile?.delay.content ??
+              animation.desktop?.delay.content ??
+              0.2,
+          }}
+          viewport={{ once: animation.onlyShowOnce }}
         >
           {description}
         </motion.p>
