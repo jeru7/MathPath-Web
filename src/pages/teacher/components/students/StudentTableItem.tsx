5187bf52195837514a4b3efefa7a06b20323cd79
jeru7 <john.ungab7@gmail.com>
2025-05-20 00:33:14 +0800

    edit: last played display

--- src/pages/teacher/components/students/StudentTableItem.tsx
+++ src/pages/teacher/components/students/StudentTableItem.tsx
@@ -2,13 +2,16 @@ import { useEffect, useMemo, useState, type ReactElement } from "react";
 
 import { Circle, Settings } from "lucide-react";
 import { format } from "date-fns";
+import { formatInTimeZone, formatInTimezone } from "date-fns-tz";
 
 import { IStudent, StudentStatusType } from "../../../../types/student.type";
 import { useTeacherContext } from "../../../../hooks/useTeacher";
 import { ISection } from "../../../../types/section.type";
 import { convertToPhilippinesDate } from "../../../../utils/date.util";
 
-interface IStudentTableItemProps { student: IStudent; onClick: (studentId: string) => void;
+interface IStudentTableItemProps {
+  student: IStudent;
+  onClick: (studentId: string) => void;
 }
 
 const formatFirstName = (student: IStudent) => {
@@ -44,44 +47,48 @@ export default function StudentTableItem({
   }, [onlineStudentIds, student._id]);
 
   return (
-    <tr
-      className="hover:bg-[var(--primary-gray)]/10 cursor-pointer text-center"
-      onClick={() => onClick(student._id)}
-    >
-      <td className="px-4 py-2 text-left">{student.studentNumber}</td>
-      <td className="max-w-[200px] truncate px-4 py-2 text-left">
-        <div className="flex items-center gap-2">
-          <Circle className="h-10 w-10" />
-          <p>{`${student.lastName}, ${formatFirstName(student)}`}</p>
-        </div>
-      </td>
-      <td className="px-4 py-2 text-center">
-        {getSectionName(student.section)}
-      </td>
-      <td
-        className={`px-4 py-2 font-bold ${status === "Online" ? "text-[var(--tertiary-green)]" : "text-[var(--primary-red)]"}`}
+    <>
+      {console.log(student)}
+      <tr
+        className="hover:bg-[var(--primary-gray)]/10 cursor-pointer text-center"
+        onClick={() => onClick(student._id)}
       >
-        {status}
-      </td>
-      <td className="px-4 py-2">
-        {format(
-          convertToPhilippinesDate(student.createdAt.toString()),
-          "MMMM d, yyyy",
-        )}
-      </td>
-      <td className="px-4 py-2">
-        {student.lastPlayed
-          ? format(
-              convertToPhilippinesDate(student.lastPlayed?.toString()),
-              "MMMM d, yyyy",
-            )
-          : "N/A"}
-      </td>
-      <td className="px-4 py-2">
-        <div className="hover:scale-101 flex w-full cursor-pointer items-center justify-center hover:text-[var(--primary-green)]">
-          <Settings />
-        </div>
-      </td>
-    </tr>
+        <td className="px-4 py-2 text-left">{student.studentNumber}</td>
+        <td className="max-w-[200px] truncate px-4 py-2 text-left">
+          <div className="flex items-center gap-2">
+            <Circle className="h-10 w-10" />
+            <p>{`${student.lastName}, ${formatFirstName(student)}`}</p>
+          </div>
+        </td>
+        <td className="px-4 py-2 text-center">
+          {getSectionName(student.section)}
+        </td>
+        <td
+          className={`px-4 py-2 font-bold ${status === "Online" ? "text-[var(--tertiary-green)]" : "text-[var(--primary-red)]"}`}
+        >
+          {status}
+        </td>
+        <td className="px-4 py-2">
+          {format(
+            convertToPhilippinesDate(student.createdAt.toString()),
+            "MMMM d, yyyy",
+          )}
+        </td>
+        <td className="px-4 py-2">
+          {student.lastPlayed
+            ? formatInTimeZone(
+                student.lastPlayed?.toString(),
+                "UTC",
+                "MMMM d, yyyy",
+              )
+            : "N/A"}
+        </td>
+        <td className="px-4 py-2">
+          <div className="hover:scale-101 flex w-full cursor-pointer items-center justify-center hover:text-[var(--primary-green)]">
+            <Settings />
+          </div>
+        </td>
+      </tr>
+    </>
   );
 }
