57b07531eb33da5db86ba9de102c81edd9c57af0
jeru7 <john.ungab7@gmail.com>
2025-07-06 00:09:42 +0800

    edit: new ui create assessment setup

--- src/modules/teacher/pages/assessments_page/components/create_assessment/CreateAssessment.tsx
+++ src/modules/teacher/pages/assessments_page/components/create_assessment/CreateAssessment.tsx
@@ -1,213 +1,110 @@
-import { useState, type ReactElement } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import * as assessmentType from "../../../../../core/types/assessment/assessment.types";
-
-import DatePicker from "react-datepicker";
+import { type ReactElement } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import "../../../../../core/styles/customDatePopper.css";
-
-import Select, { MultiValue } from "react-select";
-import { CiCalendar } from "react-icons/ci";
-import { SectionSelection } from "../../../../../core/types/section/section.types";
-import { getCustomSelectColor } from "../../../../../core/styles/selectStyles";
-import AssessmentQuestion from "./AssessmentQuestion";
+import { TbEdit } from "react-icons/tb";
+import { MdDragIndicator } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { BsPatchQuestionFill } from "react-icons/bs";
+import { CiImageOn } from "react-icons/ci";
+import { MdOutlineTextSnippet } from "react-icons/md";
 
 export default function CreateAssessment(): ReactElement {
-  const navigate = useNavigate();
-  const { teacherId } = useParams();
-
-  const [assessmentDetails, setAssessmentDetails] =
-    useState<assessmentType.CreateAssessment>({
-      name: "",
-      topic: "",
-      description: "",
-      teacher: teacherId?.toString() || "",
-      sections: [],
-      questions: [],
-      deadline: new Date(),
-    });
-
-  const [questions, setQuestions] = useState<
-    assessmentType.AssessmentQuestion[]
-  >([]);
-
-  const handleBackBtn = () => {
-    navigate("..");
-  };
-
-  const addQuestion = () => {
-    setQuestions((prev) => [
-      ...prev,
-      {
-        question: "",
-        points: 1,
-        choices: [""],
-        answer: "",
-      },
-    ]);
-  };
-
-  const updateQuestion = (
-    id: number,
-    updatedData: Partial<assessmentType.AssessmentQuestion>,
-  ) => {
-    setQuestions((prev) =>
-      prev.map((q, index) => (index === id ? { ...q, ...updatedData } : q)),
-    );
-  };
-
-  const deleteQuestion = (id: number) => {
-    setQuestions((prev) => prev.filter((_, index) => index !== id));
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setAssessmentDetails((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setAssessmentDetails((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleSectionChange = (
-    selectedOptions: MultiValue<SectionSelection>,
-  ) => {
-    const selectedSections = selectedOptions.map((option) => option.value);
-    setAssessmentDetails((prev) => ({
-      ...prev,
-      sections: selectedSections,
-    }));
-  };
-
-  const handleDateChange = (date: Date | null) => {
-    setAssessmentDetails((prev) => ({
-      ...prev,
-      deadline: date || new Date(),
-    }));
-  };
-
   return (
-    <div className="flex h-fit min-h-full w-full flex-col gap-4 bg-inherit p-4">
-      <div className="flex justify-between">
-        <div>
-          <button
-            onClick={handleBackBtn}
-            className="border-1 px-4 py-1 opacity-80 rounded-sm transition-opacity duration-200 hover:cursor-pointer hover:opacity-100"
-          >
-            Back
+    <main className="flex h-full w-full flex-col gap-2 bg-inherit p-4">
+      <header className="flex w-full items-center justify-between py-1">
+        <h3 className="text-2xl font-bold">Create Assessment</h3>
+      </header>
+
+      <div className="flex h-full flex-col">
+        <section className="flex justify-center relative">
+          <button className="absolute py-1 px-4 border rounded-sm left-0 top-1/2 -translate-y-1/2">
+            <p>Back</p>
           </button>
-        </div>
-      </div>
-
-      <main className="flex flex-col gap-4 px-32">
-        <section className="bg-white rounded-sm shadow-sm py-4 px-8 flex flex-col gap-2">
-          <div className="mb-4">
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={assessmentDetails.name}
-              onChange={handleInputChange}
-              className="w-full p-2 text-3xl border-b-2"
-              placeholder="Title"
-            />
-          </div>
-
-          <div className="mb-4">
-            <input
-              type="text"
-              id="topic"
-              name="topic"
-              value={assessmentDetails.topic}
-              onChange={handleInputChange}
-              className="w-full p-2 text-xl border-b-2"
-              placeholder="Enter topic"
-            />
-          </div>
-
-          <div className="mb-4">
-            <textarea
-              id="description"
-              name="description"
-              value={assessmentDetails.description}
-              onChange={handleTextAreaChange}
-              className="w-full p-2 text-xl border-b-2"
-              placeholder="Enter description"
-            />
-          </div>
-
-          <div className="w-full flex gap-4">
-            <div className="mb-4 w-full">
-              <label htmlFor="sections" className="block font-semibold">
-                Sections
-              </label>
-              <Select<SectionSelection, true>
-                isMulti
-                name="sections"
-                options={[
-                  { value: "section1", label: "Section 1" },
-                  { value: "section2", label: "Section 2" },
-                  { value: "section3", label: "Section 3" },
-                ]}
-                value={assessmentDetails.sections.map((section) => ({
-                  value: section,
-                  label: section.charAt(0).toUpperCase() + section.slice(1),
-                }))}
-                onChange={handleSectionChange}
-                styles={getCustomSelectColor()}
-                placeholder="Select sections..."
-              />
-            </div>
-            <div>
-              <label htmlFor="duedate" className="block font-semibold">
-                Due Date
-              </label>
-              <DatePicker
-                selected={assessmentDetails.deadline}
-                onChange={handleDateChange}
-                minDate={new Date()}
-                className="w-full shadow"
-                disabledKeyboardNavigation
-                placeholderText="Select due date"
-                dateFormat="yyyy-MM-dd"
-                customInput={
-                  <button className="flex items-center justify-between gap-4 w-full px-4 py-2 bg-[var(--tertiary-green)] text-black rounded-md hover:bg-[var(--primary-green)] transition-colors duration-200 hover:cursor-pointer">
-                    <span className="">
-                      {assessmentDetails.deadline.toLocaleDateString()}
-                    </span>
-                    <CiCalendar className="" size={20} />
-                  </button>
-                }
-              />
-            </div>
-          </div>
+          <section className="flex rounded-sm">
+            {/* Step 1 */}
+            <article className="border-t border-l border-gray-300 flex px-4 py-2 rounded-tl-sm gap-2 bg-white items-center w-[200px]">
+              <div className="flex">
+                <div className="flex items-center justify-center bg-[var(--primary-yellow)] w-10 h-10 rounded-full">
+                  <p className="text-white text-xl font-bold">1</p>
+                </div>
+              </div>
+              <div className="flex flex-col font-semibold text-lg">
+                <p className="text-base font-semibold">Create</p>
+              </div>
+            </article>
+            {/* Step 2 */}
+            <article className="border border-gray-300 flex px-4 py-2 gap-2 items-center w-[200px] opacity-50">
+              <div className="flex">
+                <div className="flex items-center justify-center bg-gray-300 w-10 h-10 rounded-full">
+                  <p className="text-white text-xl font-bold">2</p>
+                </div>
+              </div>
+              <div className="flex flex-col text-lg">
+                <p className="text-base font-semibold">Configure</p>
+              </div>
+            </article>
+            {/* Step 3 */}
+            <article className="flex px-4 rounded-e-sm border-t border-r border-b border-gray-300 gap-2 items-center w-[200px] opacity-50">
+              <div className="flex">
+                <div className="flex items-center justify-center bg-gray-300 w-10 h-10 rounded-full">
+                  <p className="text-white text-xl font-bold">3</p>
+                </div>
+              </div>
+              <div className="flex flex-col font-semibold text-lg">
+                <p className="text-base font-semibold">Publish</p>
+              </div>
+            </article>
+          </section>
         </section>
+        <section className="bg-white shadow-sm rounded-sm h-full px-96 py-12 flex flex-col gap-4">
+          {/* Initial page */}
+          <article className="flex flex-col rounded-t-sm border border-gray-300 rounded-b-sm">
+            <header className="flex justify-between items-center p-4 rounded-t-xs bg-[var(--tertiary-green)]">
+              <p className="text-white font-semibold">Page: 1</p>
+              <div className="flex items-center gap-2">
+                <button className="text-gray-100 hover:cursor-pointer hover:text-white transition-colors duration-200">
+                  <TbEdit className="h-6 w-6" />
+                </button>
+                <button className="text-gray-100 hover:cursor-pointer hover:text-white transition-colors duration-200">
+                  <MdDragIndicator className="h-6 w-6" />
+                </button>
+                <button className="text-gray-100 hover:cursor-pointer hover:text-white transition-colors duration-200">
+                  <IoClose className="h-6 w-6" />
+                </button>
+              </div>
+            </header>
+            <section className="bg-white rounded-b-xs p-4">
+              <div className="flex justify-center gap-8">
+                <button className="text-gray-400 border border-gray-400 flex gap-1 items-center rounded-sm py-2 px-5 hover:cursor-pointer hover:text-gray-600 transition-colors duration-200">
+                  <BsPatchQuestionFill />
+                  <p>Add question</p>
+                </button>
+                <button className="text-gray-400 border border-gray-400 flex gap-1 items-center rounded-sm py-2 px-5 hover:cursor-pointer hover:text-gray-600 transition-colors duration-200">
+                  <CiImageOn />
+                  <p>Add image</p>
+                </button>
+                <button className="text-gray-400 border border-gray-400 flex gap-1 items-center rounded-sm py-2 px-5 hover:cursor-pointer hover:text-gray-600 transition-colors duration-200">
+                  <MdOutlineTextSnippet />
+                  <p>Add text</p>
+                </button>
+              </div>
+            </section>
+          </article>
 
-        {/* Questions */}
-        {questions.map((question, index) => (
-          <AssessmentQuestion
-            key={index}
-            questionIndex={`question-${index}`}
-            question={question}
-            updateQuestion={(updatedData) => updateQuestion(index, updatedData)}
-            deleteQuestion={() => deleteQuestion(index)}
-          />
-        ))}
-
-        <button
-          onClick={addQuestion}
-          className="bg-white px-4 py-2 rounded-sm self-end shadow-sm hover:cursor-pointer font-semibold opacity-80 hover:opacity-100 transition-opacity duration-200"
-        >
-          + Add Question
-        </button>
-      </main>
-    </div>
+          {/* add page and next step button */}
+          <section className="flex justify-center relative">
+            <button className="flex gap-1 items-center justify-center text-gray-300 border-gray-300 border rounded-full w-10 h-10 hover:cursor-pointer hover:text-gray-500 hover:border-gray-500 transition-all duration-200">
+              <FaPlus className="w-3 h-3" />
+            </button>
+            <button className="flex items-center gap-1 py-1 px-3 text-white bg-[var(--primary-green)]/80 rounded-sm absolute right-0 top-1/2 -translate-y-1/2 hover:cursor-pointer hover:bg-[var(--primary-green)] transition-colors duration-200">
+              <p className="text-base font-semibold">Next</p>
+              <MdKeyboardArrowRight className="h-4 w-4" />
+            </button>
+          </section>
+        </section>
+      </div>
+    </main>
   );
 }
