c665c43a739dfb2f1870a928e34d46e210e83cec
jeru7 <john.ungab7@gmail.com>
2025-09-05 20:18:05 +0800

    edit: form buttons

--- src/modules/core/components/buttons/FormButtons.tsx
+++ src/modules/core/components/buttons/FormButtons.tsx
@@ -1,22 +1,34 @@
-import { type ReactElement } from "react"
+import { type ReactElement } from "react";
 
 interface IFormButtonsProps {
-  handleBack: () => void,
-  text: string,
-  disabled: boolean,
+  handleBack: () => void;
+  text: string;
+  disabled: boolean;
 }
 
-export default function FormButtons({ handleBack, text, disabled }: IFormButtonsProps): ReactElement {
-  return <>
-    <div className="flex w-full justify-end gap-4 text-lg">
-      <button type="button"
-        onClick={handleBack}
-        className="border-1 rounded-lg px-4 py-1 opacity-80 transition-opacity duration-200 hover:cursor-pointer hover:opacity-100">
-        Back</button>
-      <button type="submit"
-        disabled={disabled}
-        className="border-1 bg-[var(--primary-green)]/90 rounded-lg px-8 py-2 text-white transition-colors duration-200 hover:cursor-pointer hover:bg-[var(--primary-green)]">
-        {text}</button>
-    </div>
-  </>
+export default function FormButtons({
+  handleBack,
+  text,
+  disabled,
+}: IFormButtonsProps): ReactElement {
+  return (
+    <>
+      <div className="flex w-full justify-between gap-4">
+        <button
+          type="button"
+          onClick={handleBack}
+          className="opacity-80 transition-opacity duration-200 hover:cursor-pointer hover:opacity-100"
+        >
+          <p className="underline">Cancel</p>
+        </button>
+        <button
+          type="submit"
+          disabled={disabled}
+          className="border-1 bg-[var(--primary-green)] opacity-80 rounded-lg px-5 py-2 text-white hover:cursor-pointer hover:opacity-100 transition-opacity duration-200"
+        >
+          {text}
+        </button>
+      </div>
+    </>
+  );
 }
