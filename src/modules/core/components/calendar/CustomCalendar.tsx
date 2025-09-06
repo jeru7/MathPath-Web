1539cd000403c6b170978040b525578a7a2b9b21
jeru7 <john.ungab7@gmail.com>
2025-09-06 18:56:41 +0800

    fix: calendar ui bug

--- src/modules/core/components/calendar/CustomCalendar.tsx
+++ src/modules/core/components/calendar/CustomCalendar.tsx
@@ -14,7 +14,7 @@ export default function CustomCalendar({
     <article
       className={`${classes} w-full rounded-sm bg-white shadow-sm p-4 flex flex-col gap-8 items-center`}
     >
-      <div className="h-full">
+      <div className="h-full w-full">
         <Calendar showFixedNumberOfWeeks={true} />
       </div>
     </article>
