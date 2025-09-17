255bc522b8886a2fce0594e54ef1a45923548ee3
jeru7 <john.ungab7@gmail.com>
2025-09-17 21:40:04 +0800

    add: forgot pass set new password

--- src/modules/core/components/auth/forgot-password/SetNewPassword.tsx
+++ src/modules/core/components/auth/forgot-password/SetNewPassword.tsx
@@ -1,86 +1,45 @@
-import { useEffect, useState, type ReactElement } from "react";
+import { type ReactElement, useEffect, useState } from "react";
 import { FaEye, FaEyeSlash, FaUserLock } from "react-icons/fa";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../../../../auth/contexts/auth.context";
 import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { changePasswordService } from "../../../../auth/services/auth.service";
+import {
+  SetNewPasswordDTO,
+  SetNewPasswordSchema,
+} from "../../../../student/types/student-forgot-pass.schema";
+import mathPathTitle from "../../../../../assets/svgs/mathpath-title.svg";
 
 export default function SetNewPassword(): ReactElement {
-  const { changePassword } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
 
   const email = (location.state as { email: string })?.email;
 
-  const [errors, setErrors] = useState<string | null | boolean>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SetNewPasswordDTO>({
+    resolver: zodResolver(SetNewPasswordSchema),
+    defaultValues: {
+      newPassword: "",
+      confirmNewPassword: "",
+    },
+  });
 
-  const [newPassword, setNewPassword] = useState<string>("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
-
-  const [disableConfirmPassword, setDisableConfirmPassword] =
-    useState<boolean>(true);
-
-  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmationPassword, setShowConfirmationPassword] =
-    useState<boolean>(false);
+    useState(false);
 
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    let updatedNewPassword = newPassword;
-    let updatedConfirmPassword = confirmNewPassword;
-
-    setErrors(null);
-
-    if (name === "newPassword") {
-      updatedNewPassword = value;
-      setNewPassword(value);
-    } else if (name === "confirmNewPassword") {
-      updatedConfirmPassword = value;
-      setConfirmNewPassword(value);
-    }
-
-    if (
-      updatedNewPassword === updatedConfirmPassword &&
-      updatedNewPassword !== "" &&
-      updatedConfirmPassword !== ""
-    ) {
-      setDisableConfirmPassword(false);
-    } else {
-      setDisableConfirmPassword(true);
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (newPassword === "" || confirmNewPassword === "") {
-      return;
-    }
-
-    if (newPassword.length < 8) {
-      setErrors("Password must have at least 8 characters");
-      return;
-    }
-
-    if (!/[!@#$%^&*(),.?":{}|<>_\-+=~`[\]\\;'/]/.test(newPassword)) {
-      setErrors("Password must contain at least one special character");
-      return;
-    }
-
-    if (newPassword !== confirmNewPassword) {
-      setErrors("Passwords do not match");
-      return;
-    }
-
+  const onSubmit = async (data: SetNewPasswordDTO) => {
     try {
-      await changePassword(email, newPassword);
-      navigate("/auth/login");
+      await changePasswordService(email, data.newPassword);
       toast.success("Password changed successfully");
-    } catch (err: unknown) {
-      if (err instanceof Error) {
-        setErrors("Something went wrong. Please try again.");
-      } else {
-        setErrors("Unexpected error occurred.");
-      }
+      navigate("/auth/login");
+    } catch {
+      toast.error("Something went wrong. Please try again.");
     }
   };
 
@@ -89,91 +48,76 @@ export default function SetNewPassword(): ReactElement {
   }, [email, navigate]);
 
   return (
-    <main className="transition-all duration-200 flex h-fit w-[90%] min-w-[300px] max-w-sm flex-col rounded-xl border-2 border-white/20 bg-white/10 py-4 px-8 shadow-lg backdrop-blur-lg sm:max-w-md md:max-w-lg">
+    <main className="relative w-full min-h-screen overflow-auto md:mt-20 md:min-h-0 flex gap-8 md:gap-4 md:h-fit min-w-[300px] flex-col rounded-xl border-2 border-white/20 bg-black/50 py-4 px-8 shadow-lg backdrop-blur-sm  md:max-w-lg transition-all duration-200">
       <header className="flex flex-col gap-4">
+        <img
+          src={mathPathTitle}
+          alt="MathPath Icon/Logo"
+          className="md:hidden self-center aspect-[16/9] w-[40%] min-w-[200px] max-w-[300px] hover:scale-105 hover:cursor-pointer md:w-[10vw] transition-transform duration-100"
+          onClick={() => navigate("/")}
+        />
         <FaUserLock className="self-center text-[var(--primary-yellow)] h-24 w-24" />
         <h3 className="text-white font-bold text-xl text-center mb-4">
           Change Password
         </h3>
       </header>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
         {/* new password */}
         <div className="relative">
           <input
             type={showNewPassword ? "text" : "password"}
-            name="newPassword"
-            value={newPassword}
-            onChange={(e) => handlePasswordChange(e)}
-            className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pr-12 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0 
-                ${errors ? "" : "border-transparent bg-gray-100/30 focus:border-black"}`}
             placeholder=" "
+            className="peer block w-full appearance-none rounded-lg border-2 px-2.5 pr-12 pb-2.5 pt-5 text-sm bg-gray-100/30 text-[var(--primary-black)] focus:outline-none focus:ring-0"
+            {...register("newPassword")}
           />
-          <label
-            className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 
-                ${errors ? "font-bold text-red-500" : "text-gray-500 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]"}`}
-          >
+          <label className="absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 text-gray-800 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]">
             New Password
           </label>
-
-          {/* show toggler */}
           <button
             type="button"
             onClick={() => setShowNewPassword((prev) => !prev)}
             className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-600 hover:text-black hover:cursor-pointer"
           >
-            {showNewPassword ? (
-              <FaEye className="w-5 h-5" />
-            ) : (
-              <FaEyeSlash className="w-5 h-5" />
-            )}
+            {showNewPassword ? <FaEye /> : <FaEyeSlash />}
           </button>
+          {errors.newPassword && (
+            <p className="text-xs text-red-500 mt-1">
+              {errors.newPassword.message}
+            </p>
+          )}
         </div>
 
-        {/* confirmation password */}
+        {/* confirm password */}
         <div className="relative">
           <input
             type={showConfirmationPassword ? "text" : "password"}
-            name="confirmNewPassword"
-            value={confirmNewPassword}
-            onChange={(e) => handlePasswordChange(e)}
-            className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pr-12 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0 
-                ${errors ? "" : "border-transparent bg-gray-100/30 focus:border-black"}`}
             placeholder=" "
+            className="peer block w-full appearance-none rounded-lg border-2 px-2.5 pr-12 pb-2.5 pt-5 text-sm bg-gray-100/30 text-[var(--primary-black)] focus:outline-none focus:ring-0"
+            {...register("confirmNewPassword")}
           />
-          <label
-            className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 
-                ${errors ? "font-bold text-red-500" : "text-gray-500 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]"}`}
-          >
-            Confirm New Password
+          <label className="absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 text-gray-800 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]">
+            Confirm Password
           </label>
-
-          {/* show toggler */}
           <button
             type="button"
             onClick={() => setShowConfirmationPassword((prev) => !prev)}
             className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-600 hover:text-black hover:cursor-pointer"
           >
-            {showConfirmationPassword ? (
-              <FaEye className="w-5 h-5" />
-            ) : (
-              <FaEyeSlash className="w-5 h-5" />
-            )}
+            {showConfirmationPassword ? <FaEye /> : <FaEyeSlash />}
           </button>
+          {errors.confirmNewPassword && (
+            <p className="text-xs text-red-500 mt-1">
+              {errors.confirmNewPassword.message}
+            </p>
+          )}
         </div>
 
-        {errors && (
-          <div className="p-3 bg-red-500 rounded-sm">
-            <p className="text-white">{errors}</p>
-          </div>
-        )}
-
-        {/* confirmation button */}
         <button
           type="submit"
-          className="text-base text-white font-bold transition-color duration-200 rounded-lg bg-[var(--primary-yellow)]/80 p-3 enabled:hover:bg-[var(--primary-yellow)]/100 enabled:hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
-          disabled={disableConfirmPassword}
+          disabled={isSubmitting}
+          className="text-base text-white font-bold transition-color duration-200 rounded-lg bg-[var(--primary-yellow)]/80 p-3 enabled:hover:bg-[var(--primary-yellow)]/100 enabled:hover:cursor-pointer"
         >
-          Change Password
+          {isSubmitting ? "Submitting..." : "Change Password"}
         </button>
       </form>
     </main>
