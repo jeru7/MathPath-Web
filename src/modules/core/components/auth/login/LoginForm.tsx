af4dafde106980e2afdb1d39cb388c0a4c51723a
jeru7 <john.ungab7@gmail.com>
2025-09-17 21:40:34 +0800

    edit: updated login form validation

--- src/modules/core/components/auth/login/LoginForm.tsx
+++ src/modules/core/components/auth/login/LoginForm.tsx
@@ -5,89 +5,107 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useAuth } from "../../../../auth/contexts/auth.context";
 import { AccountType } from "../../../../auth/types/auth.type";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { handleApiError } from "../../../utils/api/error.util";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  LoginFormDTO,
+  LoginFormSchema,
+} from "../../../../student/types/student.schema";
+import mathPathTitle from "../../../../../assets/svgs/mathpath-title.svg";
 
 export default function LoginForm(): ReactElement {
   const navigate = useNavigate();
   const { user, login, isLoading } = useAuth();
-  const [formData, setFormData] = useState({ identifier: "", password: "" });
-  const [errors, setErrors] = useState({ identifier: false, password: false });
-  const [loginError, setLoginError] = useState<string | null>(null);
   const [accountType, setAccountType] = useState<AccountType>("Student");
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [identifier, setIdentifier] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+    clearErrors,
+  } = useForm<LoginFormDTO>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      accountType: accountType,
+      identifier: "",
+      password: "",
+    },
+  });
 
   useEffect(() => {
     if (!isLoading && user) {
-      if (accountType === "Teacher") {
-        navigate(`/${user.role}/${user.id}`);
-      } else if (accountType === "Student") {
-        navigate(`/${user.role}/${user.id}`);
-      }
+      navigate(`/${user.role}/${user.id}`);
     }
-  }, [user, isLoading, accountType, navigate]);
+  }, [user, isLoading, navigate]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const name = e.target.name;
+    let value = e.target.value;
 
-    setErrors({ identifier: false, password: false });
-    setLoginError(null);
-
-    let newValue = value;
     if (name === "identifier" && accountType === "Student") {
-      newValue = value.replace(/\D/g, "");
+      value = value.replace(/\D/g, "");
+      if (value.length > 12) value = value.slice(0, 12);
     }
 
-    setFormData((prev) => ({ ...prev, [name]: newValue }));
+    setIdentifier(value);
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const isInputEmpty = {
-      identifier: formData.identifier.trim() === "",
-      password: formData.password.trim() === "",
-    };
-
-    setErrors(isInputEmpty);
-
-    if (isInputEmpty.identifier || isInputEmpty.password) return;
+  const onSubmit = async (data: LoginFormDTO) => {
+    setLoginError(null);
 
     try {
-      await login(formData.identifier, formData.password);
-      setLoginError(null);
+      await login(data.identifier, data.password);
     } catch (error: unknown) {
-      if (error instanceof Error) {
-        if (error.message === "Failed to log in.") {
-          if (accountType === "Teacher") {
-            setLoginError("Invalid email or password. Try again.");
-          } else if (accountType === "Student") {
-            setLoginError("Invalid LRN or password. Try again.");
-          }
-        }
+      const errorData = handleApiError(error);
+
+      if (
+        errorData.error === "INVALID_PASSWORD" ||
+        errorData.error === "INVALID_IDENTIFIER"
+      ) {
+        const message =
+          accountType === "Teacher"
+            ? "Invalid email or password. Try again."
+            : "Invalid LRN or password. Try again.";
+        setLoginError(message);
+      } else {
+        setLoginError("An unexpected error occurred. Try again later.");
       }
     }
   };
 
   return (
-    <main className="relative flex h-fit w-[90%] min-w-[300px] max-w-sm flex-col gap-4 rounded-xl border-2 border-white/20 bg-white/10 py-4 px-8 shadow-lg backdrop-blur-lg transition-all duration-200 z-30 sm:max-w-md md:max-w-lg">
+    <main className="relative w-full min-h-screen overflow-auto md:min-h-0 flex gap-4 md:h-fit min-w-[300px] flex-col rounded-xl border-2 border-white/20 bg-black/50 py-4 px-8 shadow-lg backdrop-blur-sm  md:max-w-2xl transition-all duration-200">
       <div className="flex flex-col gap-4">
+        <img
+          src={mathPathTitle}
+          alt="MathPath Icon/Logo"
+          className="md:hidden self-center aspect-[16/9] w-[40%] min-w-[200px] max-w-[300px] hover:scale-105 hover:cursor-pointer md:w-[10vw] transition-transform duration-100"
+          onClick={() => navigate("/")}
+        />
         <h3 className="text-center text-xl font-bold text-white md:text-2xl">
           Choose Account Type
         </h3>
 
         <div className="flex gap-4">
-          {/* Student Radio Button */}
+          {/* Student */}
           <div className="w-full">
             <input
               type="radio"
               name="accountType"
               id="student"
-              value={"Student"}
+              value="Student"
               className="peer hidden"
               onChange={() => {
                 setAccountType("Student");
                 setLoginError(null);
-                setFormData({ identifier: "", password: "" });
-                setErrors({ identifier: false, password: false });
+                setIdentifier("");
+                setValue("accountType", "Student");
+                clearErrors();
               }}
               defaultChecked
             />
@@ -99,19 +117,19 @@ export default function LoginForm(): ReactElement {
             </label>
           </div>
 
-          {/* Teacher Radio Button */}
+          {/* Teacher */}
           <div className="w-full">
             <input
               type="radio"
               name="accountType"
               id="teacher"
-              value={"Teacher"}
+              value="Teacher"
               className="peer hidden"
               onChange={() => {
                 setAccountType("Teacher");
                 setLoginError(null);
-                setFormData({ identifier: "", password: "" });
-                setErrors({ identifier: false, password: false });
+                setIdentifier("");
+                setValue("accountType", "Teacher");
               }}
             />
             <label
@@ -128,106 +146,117 @@ export default function LoginForm(): ReactElement {
           <p>Please fill out the form below to get started</p>
         </div>
 
-        {/* Horizontal Line */}
         <div className="w-full h-[1px] bg-gray-200/50"></div>
       </div>
 
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 text-xl">
-        {/* Login Error */}
-        <AnimatePresence>
-          {loginError && (
-            <motion.div
-              initial={{ opacity: 0, y: -8 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -8 }}
-              transition={{ duration: 0.3 }}
-              className="bg-red-300 text-red-500 rounded-md py-3 px-1 flex items-center gap-2 w-full"
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full flex-1"
+      >
+        <div className="flex flex-col justify-between flex-1 gap-4">
+          <div className="flex flex-col gap-4">
+            {/* Login Error */}
+            <AnimatePresence>
+              {loginError && (
+                <motion.div
+                  initial={{ opacity: 0, y: -8 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -8 }}
+                  transition={{ duration: 0.3 }}
+                  className="bg-red-300 text-red-500 rounded-md py-3 px-1 flex items-center gap-2 w-full"
+                >
+                  <GoShieldX className="h-8 w-8" />
+                  <p className="text-sm">{loginError}</p>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            {/* Identifier */}
+            <div className="relative">
+              <input
+                type={accountType === "Student" ? "text" : "email"}
+                {...register("identifier")}
+                value={identifier}
+                onChange={handleChange}
+                placeholder=" "
+                className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] bg-gray-100/30 focus:outline-none focus:ring-0`}
+                inputMode={accountType === "Student" ? "numeric" : undefined}
+              />
+              <label
+                className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 text-gray-800 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]`}
+              >
+                {accountType === "Student"
+                  ? "Learner Reference Number (LRN)"
+                  : "Email"}
+              </label>
+              {errors.identifier && (
+                <p className="text-xs text-red-500">
+                  {errors.identifier.message}
+                </p>
+              )}
+            </div>
+
+            {/* Password */}
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                {...register("password")}
+                placeholder=" "
+                className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] bg-gray-100/30 focus:outline-none focus:ring-0`}
+              />
+              <label
+                className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 text-gray-800 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]`}
+              >
+                Password
+              </label>
+              <button
+                type="button"
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-600 hover:text-black hover:cursor-pointer"
+              >
+                {showPassword ? (
+                  <FaEye className="w-5 h-5" />
+                ) : (
+                  <FaEyeSlash className="w-5 h-5" />
+                )}
+              </button>
+              {errors.password && (
+                <p className="text-xs text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            {/* Forgot Password */}
+            <button
+              className="text-left text-sm text-gray-100 opacity-50 transition-opacity duration-200 hover:cursor-pointer w-fit hover:underline hover:opacity-100"
+              type="button"
+              onClick={() => navigate("/auth/forgot-password")}
             >
-              <GoShieldX className="h-8 w-8" />
-              <p className="text-sm">{loginError}</p>
-            </motion.div>
-          )}
-        </AnimatePresence>
+              Forgot Password?
+            </button>
 
-        {/* Identifier - Email/LRN */}
-        <div className="relative">
-          <input
-            type={accountType === "Student" ? "text" : "email"}
-            name="identifier"
-            value={formData.identifier}
-            onChange={handleChange}
-            className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0 
-                ${errors.identifier ? "" : "border-transparent bg-gray-100/30 focus:border-black"}`}
-            placeholder=" "
-            inputMode={accountType === "Student" ? "numeric" : undefined}
-          />
-          <label
-            className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 
-                ${errors.identifier ? "font-bold text-red-500" : "text-gray-500 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]"}`}
-          >
-            {accountType === "Student" ? "LRN" : "Email"}
-          </label>
+            {/* Login Button */}
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="text-base text-white font-bold hover:bg-[var(--primary-yellow)]/100 transition-color duration-200 rounded-lg bg-[var(--primary-yellow)]/80 p-3 hover:cursor-pointer"
+            >
+              {isSubmitting ? "Logging in..." : "Login"}
+            </button>
+          </div>
+          <div className="flex gap-2 self-center">
+            <p className="text-white">Don't have an account?</p>
+            <button
+              type="button"
+              className="text-[var(--primary-yellow)] hover:cursor-pointer hover:underline"
+              onClick={() => navigate("/auth/register")}
+            >
+              Register
+            </button>
+          </div>
         </div>
-
-        {/* Password */}
-        <div className="relative">
-          <input
-            type={showPassword ? "text" : "password"}
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0 
-                ${errors.password ? "" : "border-transparent bg-gray-100/30 focus:border-black"}`}
-            placeholder=" "
-          />
-          <label
-            className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 
-                ${errors.password ? "font-bold text-red-500" : "text-gray-500 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]"}`}
-          >
-            Password
-          </label>
-          {/* show toggler */}
-          <button
-            type="button"
-            onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute right-5 top-1/2 -translate-y-1/2 text-gray-600 hover:text-black hover:cursor-pointer"
-          >
-            {showPassword ? (
-              <FaEye className="w-5 h-5" />
-            ) : (
-              <FaEyeSlash className="w-5 h-5" />
-            )}
-          </button>
-        </div>
-
-        {/* Forgot Password */}
-        <button
-          className="text-left text-sm text-gray-100 opacity-50 transition-opacity duration-200 hover:cursor-pointer w-fit hover:underline hover:opacity-100"
-          type="button"
-          onClick={() => navigate("/auth/forgot-password")}
-        >
-          Forgot Password?
-        </button>
-
-        {/* Login Button */}
-        <button
-          type="submit"
-          className="text-base text-white font-bold hover:bg-[var(--primary-yellow)]/100 transition-color duration-200 rounded-lg bg-[var(--primary-yellow)]/80 p-3 hover:cursor-pointer"
-        >
-          Login
-        </button>
       </form>
-
-      <div className="flex gap-2 absolute -bottom-10 left-1/2 -translate-x-1/2">
-        <p className="text-white">Don't have an account?</p>
-        <button
-          type="button"
-          className="text-[var(--primary-yellow)] hover:cursor-pointer hover:underline"
-          onClick={() => navigate("/auth/register")}
-        >
-          Register
-        </button>
-      </div>
     </main>
   );
 }
