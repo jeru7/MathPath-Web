ec5efd96360e5eefeac14bc047cdbb49531eaeac
jeru7 <john.ungab7@gmail.com>
2025-09-17 21:39:57 +0800

    add: forgot pass request code

--- src/modules/core/components/auth/forgot-password/RequestCode.tsx
+++ src/modules/core/components/auth/forgot-password/RequestCode.tsx
@@ -1,50 +1,59 @@
-import { FormEvent, useState, type ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 import { FaAngleLeft } from "react-icons/fa6";
 import { PiShieldWarningDuotone } from "react-icons/pi";
-import { useAuth } from "../../../../auth/contexts/auth.context";
 import { useNavigate } from "react-router-dom";
+import { requestPasswordResetCodeService } from "../../../../auth/services/auth.service";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  RequestCodeDTO,
+  RequestCodeSchema,
+} from "../../../../student/types/student-forgot-pass.schema";
+import mathPathTitle from "../../../../../assets/svgs/mathpath-title.svg";
 
 export default function RequestCode(): ReactElement {
-  const [email, setEmail] = useState("");
-  const [errors, setErrors] = useState<string | boolean | null>(null);
-  const { requestPasswordResetCode, isLoading } = useAuth();
   const navigate = useNavigate();
+  const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    setErrors(null);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RequestCodeDTO>({
+    resolver: zodResolver(RequestCodeSchema),
+    defaultValues: { email: "" },
+  });
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (email === "") {
-      setErrors(true);
-      return;
-    }
-
+  const onSubmit = async (data: RequestCodeDTO) => {
+    setServerError(null);
     try {
-      await requestPasswordResetCode(email);
-      navigate("/auth/forgot-password/verify-code", { state: { email } });
+      await requestPasswordResetCodeService(data.email);
+      navigate("/auth/forgot-password/verify-code", {
+        state: { email: data.email },
+      });
     } catch (err: unknown) {
       if (err instanceof Error) {
         if (err.message === "EMAIL_NOT_FOUND") {
-          setErrors("We can't find that email.");
+          setServerError("We can't find that email.");
         } else {
-          setErrors("Something went wrong. Please try again.");
+          setServerError("Something went wrong. Please try again.");
         }
       } else {
-        setErrors("Unexpected error occurred.");
+        setServerError("Unexpected error occurred.");
       }
     }
   };
 
-  if (isLoading) return <p>Loading...</p>;
-
   return (
-    <main className="transition-all duration-200 flex h-fit w-[90%] min-w-[300px] max-w-sm flex-col rounded-xl border-2 border-white/20 bg-white/10 py-4 px-8 shadow-lg backdrop-blur-lg sm:max-w-md md:max-w-lg">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        {/* header */}
+    <main className="relative w-full min-h-screen overflow-auto md:min-h-0 flex gap-4 md:h-fit min-w-[300px] flex-col rounded-xl border-2 border-white/20 bg-black/50 py-4 px-8 shadow-lg backdrop-blur-sm  md:max-w-lg transition-all duration-200">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        {/* Header */}
+        <img
+          src={mathPathTitle}
+          alt="MathPath Icon/Logo"
+          className="md:hidden self-center aspect-[16/9] w-[40%] min-w-[200px] max-w-[300px] hover:scale-105 hover:cursor-pointer md:w-[10vw] transition-transform duration-100"
+          onClick={() => navigate("/")}
+        />
         <div className="flex flex-col items-center gap-4">
           <PiShieldWarningDuotone className="w-24 h-24 text-[var(--primary-yellow)]" />
           <h3 className="font-bold text-xl text-white">Forgot Password</h3>
@@ -53,45 +62,42 @@ export default function RequestCode(): ReactElement {
           </p>
         </div>
 
-        {/* inputs */}
-        <div>
-          <div className="relative">
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => handleChange(e)}
-              className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0
-                ${errors ? "" : "border-transparent bg-gray-100/30 focus:border-black"}`}
-              placeholder=" "
-            />
-            <label
-              className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300
-                ${errors ? "font-bold text-red-500" : "text-gray-500 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]"}`}
-            >
-              Email
-            </label>
-          </div>
+        {/* Email Input */}
+        <div className="relative">
+          <input
+            type="email"
+            placeholder=" "
+            className={`peer block w-full appearance-none rounded-lg border-2 px-2.5 pb-2.5 pt-5 text-sm text-[var(--primary-black)] focus:outline-none focus:ring-0 bg-gray-100/30`}
+            {...register("email")}
+          />
+          <label
+            className={`absolute start-2.5 top-4 z-10 origin-[0] -translate-y-4 scale-75 transform text-sm duration-300 text-gray-800 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:font-bold peer-focus:text-[var(--primary-black)]`}
+          >
+            Email
+          </label>
+          {errors.email && (
+            <p className="text-xs text-red-500 mt-1">{errors.email.message}</p>
+          )}
         </div>
 
-        {errors && typeof errors === "string" ? (
-          <p className="text-red-500 font-semibold text-sm">{errors}</p>
-        ) : null}
+        {/* Server error */}
+        {serverError && (
+          <p className="text-xs text-red-500 font-semibold">{serverError}</p>
+        )}
 
-        {/* buttons */}
+        {/* Buttons */}
         <div className="flex flex-col gap-8">
-          {/* submit button */}
           <button
             type="submit"
+            disabled={isSubmitting}
             className="text-white text-base font-bold hover:bg-[var(--primary-yellow)]/100 transition-color duration-200 rounded-lg bg-[var(--primary-yellow)]/80 py-2 hover:cursor-pointer"
           >
-            Submit
+            {isSubmitting ? "Submitting..." : "Submit"}
           </button>
 
-          {/* back to login button */}
           <button
             type="button"
-            className="font-bold text-gray-100 flex gap-2 items-center w-fit self-center opacity-80 hover:opacity-100 transition-opacity duration-200 hover:cursor-pointer"
+            className={`font-bold text-gray-100 gap-2 items-center w-fit self-center opacity-80 hover:opacity-100 transition-opacity duration-200 hover:cursor-pointer ${isSubmitting ? "hidden" : "flex"}`}
             onClick={() => navigate("/auth/login")}
           >
             <FaAngleLeft />
