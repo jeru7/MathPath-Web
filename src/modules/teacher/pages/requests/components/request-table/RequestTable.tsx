bf5ca959f2e13638ea686ddb7960ec2eee81f195
jeru7 <john.ungab7@gmail.com>
2025-10-22 09:16:36 +0800

    fix: last name typo

--- src/modules/teacher/pages/requests/components/request-table/RequestTable.tsx
+++ src/modules/teacher/pages/requests/components/request-table/RequestTable.tsx
@@ -44,7 +44,7 @@ export default function RequestTable({
         request.accountInfo?.firstName
           ?.toLowerCase()
           .includes(searchTerm.toLowerCase()) ||
-        request.accountInfo?.lastname
+        request.accountInfo?.lastName
           ?.toLowerCase()
           .includes(searchTerm.toLowerCase()) ||
         request.accountInfo?.email
