5adfed67c8581c02a4c3388ea5bff91819d491b9
jeru7 <john.ungab7@gmail.com>
2025-10-27 01:13:53 +0800

    edit: added the components for their specific path on admin routes

--- src/modules/admin/routes/adminRoutesConfig.tsx
+++ src/modules/admin/routes/adminRoutesConfig.tsx
@@ -1,6 +1,7 @@
 import Admin from "../components/admin/Admin";
-import Dashboard from "../components/dashboard/Dashboard";
+import Dashboard from "../pages/dashboard/Dashboard";
 import Profile from "../pages/profile/Profile";
+import Sections from "../pages/sections/Sections";
 import AdminSettings from "../pages/settings/AdminSettings";
 import Statistics from "../pages/statistics/Statistics";
 import Students from "../pages/students/Students";
@@ -13,11 +14,31 @@ export const adminRoutesConfig = [
     children: [
       { index: true, element: <Dashboard /> },
       { path: "add-teacher", element: <Dashboard /> },
-      { path: "teachers", element: <Teachers /> },
-      { path: "students", element: <Students /> },
+      {
+        path: "teachers",
+        children: [
+          { index: true, element: <Teachers /> },
+          { path: "add-teacher", element: <Teachers /> },
+          { path: ":teacherId", element: <Teachers /> },
+        ],
+      },
+      {
+        path: "students",
+        children: [
+          { index: true, element: <Students /> },
+          { path: "add-student", element: <Students /> },
+        ],
+      },
       { path: "profile", element: <Profile /> },
       { path: "settings", element: <AdminSettings /> },
       { path: "statistics", element: <Statistics /> },
+      {
+        path: "sections",
+        children: [
+          { index: true, element: <Sections /> },
+          { path: "add-section", element: <Sections /> },
+        ],
+      },
     ],
   },
 ];
