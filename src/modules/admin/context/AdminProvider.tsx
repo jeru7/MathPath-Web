d9c8ce43a30c4164778fb84c2ea2200c7d2c1686
jeru7 <john.ungab7@gmail.com>
2025-10-27 00:23:21 +0800

    add: websocket setup for admin

--- src/modules/admin/context/AdminProvider.tsx
+++ src/modules/admin/context/AdminProvider.tsx
@@ -1,7 +1,18 @@
-import { type ReactElement } from "react";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  type ReactElement,
+} from "react";
 import { AdminContext } from "./admin.context";
 import { useAdmin, useAdminTeacher } from "../services/admin.service";
-import { useAdminStudents } from "../services/admin-student";
+import { useAdminSections } from "../services/admin-section.service";
+import { WSS } from "../../core/constants/api.constant";
+import { useQueryClient } from "@tanstack/react-query";
+import { useAdminAssessments } from "../services/admin-assessment.service";
+import { useAdminStudents } from "../services/admin-student.service";
 
 export default function AdminProvider({
   adminId,
@@ -10,15 +21,133 @@ export default function AdminProvider({
   adminId: string;
   children: React.ReactNode;
 }): ReactElement {
+  const wsRef = useRef<WebSocket | null>(null);
+  const reconnectAttempts = useRef(0);
+  const maxReconnectAttempts = 5;
+  const reconnectInterval = 3000;
+  const isMounted = useRef(true);
+
+  const queryClient = useQueryClient();
+
   const { data: admin } = useAdmin(adminId);
   const { data: teachers } = useAdminTeacher(adminId);
   const { data: students } = useAdminStudents(adminId);
+  const { data: sections } = useAdminSections(adminId);
+  const { data: assessments } = useAdminAssessments(adminId);
+
+  // track student online status
+  const [onlineStudentIds, setOnlineStudentIds] = useState<string[]>([]);
+
+  const onlineStudents = useMemo(() => {
+    return students?.filter((s) => onlineStudentIds.includes(s.id));
+  }, [students, onlineStudentIds]);
+
+  const connectWebSocket = useCallback(() => {
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      return;
+    }
+
+    if (wsRef.current) {
+      wsRef.current.close();
+    }
+
+    wsRef.current = new WebSocket(WSS);
+    // console.log(WSS);
+    const ws = wsRef.current;
+
+    ws.onopen = () => {
+      if (!isMounted.current) {
+        ws.close();
+        return;
+      }
+
+      // console.log("WebSocket connected");
+
+      reconnectAttempts.current = 0;
+    };
+
+    ws.onmessage = (e) => {
+      if (!isMounted.current) return;
+      const { type, data } = JSON.parse(e.data);
+      // console.log("Received message: ", type, data);
+
+      if (type === "TEACHER_INITIAL_DATA") {
+        setOnlineStudentIds(data.onlineStudents);
+      }
+
+      if (type === "STUDENT_ONLINE") {
+        setOnlineStudentIds((prev) =>
+          prev.includes(data.studentId) ? prev : [...prev, data.studentId],
+        );
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "active-students"],
+        });
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "online-trend"],
+        });
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "students"],
+        });
+      }
+
+      if (type === "STUDENT_OFFLINE") {
+        setOnlineStudentIds((prev) =>
+          prev.filter((id) => id !== data.studentId),
+        );
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "active-students"],
+        });
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "online-trend"],
+        });
+        queryClient.refetchQueries({
+          queryKey: ["admin", adminId, "students"],
+        });
+      }
+    };
+
+    ws.onclose = () => {
+      if (!isMounted.current) return;
+
+      // console.log("WebSocket closed");
+
+      if (reconnectAttempts.current < maxReconnectAttempts) {
+        reconnectAttempts.current += 1;
+        // console.log(
+        //   `Attempting to reconnect (${reconnectAttempts.current}/${maxReconnectAttempts})...`,
+        // );
+        setTimeout(connectWebSocket, reconnectInterval);
+      }
+    };
+
+    wsRef.current.onerror = () => {
+      if (!isMounted.current) return;
+
+      // console.error("WebSocket error:", error);
+    };
+  }, [adminId, queryClient]);
+
+  useEffect(() => {
+    isMounted.current = true;
+    connectWebSocket();
+
+    return () => {
+      isMounted.current = false;
+      if (wsRef.current?.readyState === WebSocket.OPEN) {
+        wsRef.current.close();
+        wsRef.current = null;
+      }
+    };
+  }, [connectWebSocket]);
 
   const value = {
     adminId,
     admin: admin || null,
     teachers: teachers || [],
     students: students || [],
+    sections: sections || [],
+    assessments: assessments || [],
+    onlineStudents: onlineStudents || [],
   };
 
   return (
--- src/modules/admin/context/admin.context.ts
+++ src/modules/admin/context/admin.context.ts
@@ -2,12 +2,17 @@ import { createContext, useContext } from "react";
 import { Admin } from "../types/admin.type";
 import { Teacher } from "../../teacher/types/teacher.type";
 import { Student } from "../../student/types/student.type";
+import { Section } from "../../core/types/section/section.type";
+import { Assessment } from "../../core/types/assessment/assessment.type";
 
 type AdminContextType = {
   adminId: string;
   admin: Admin | null;
   teachers: Teacher[];
   students: Student[];
+  sections: Section[];
+  assessments: Assessment[];
+  onlineStudents: Student[];
 };
 
 export const AdminContext = createContext<AdminContextType | undefined>(
