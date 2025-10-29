262c23ab7e5ec7fe0db7f3df327eb816a4da0b38
jeru7 <john.ungab7@gmail.com>
2025-10-30 03:50:27 +0800

    edit: modal overlay size
    - updated the assessment details modal on student's

--- src/modules/student/pages/assessments/components/AssessmentDetailsModal.tsx
+++ src/modules/student/pages/assessments/components/AssessmentDetailsModal.tsx
@@ -220,7 +220,7 @@ export default function AssessmentDetailsModal({
   return (
     <>
       <ModalOverlay isOpen={isOpen} onClose={onClose}>
-        <div className="bg-white border border-white dark:border-gray-700 dark:bg-gray-800 rounded-sm shadow-sm h-[100svh] w-[100svw] md:h-[85svh] md:max-w-5xl overflow-hidden flex flex-col">
+        <div className="bg-white border border-white dark:border-gray-700 dark:bg-gray-800 rounded-sm h-[100svh] w-[100svw] md:h-[85svh] md:w-[90svw] lg:w-[75svw] md:max-w-7xl md:max-h-[800px] overflow-hidden flex flex-col">
           {/* header */}
           <header className="flex items-center justify-between p-4 border-b border-gray-200 dark:border-gray-700">
             <div className="flex items-center gap-3 flex-1 min-w-0">
